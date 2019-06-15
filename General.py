98a9d92e075e5604e4e050c8b265122e53085d7d
Mike Skrypek <mikeskrypekjr@comcast.net>
2019-06-14 19:51:36 -0500

    more practice with loops

--- General.py
+++ General.py
@@ -1,4 +1,3 @@
-import math
 import random
 
 
@@ -6,8 +5,27 @@ secretNumb = random.randint(1, 9)
 
 guessCount = 0
 guessLimit = 3
+goAgain = True
 
-while guessCount < guessLimit:
-    guess = int(input("Enter your guess: "))
-    guessCount += 1
+while goAgain:
+    print("Guess a number between 1 and 9")
+    while guessCount < guessLimit:
+        guess = int(input("Enter your guess: "))
+        guessCount += 1
+        if guess == secretNumb:
+            print(F"You guess correct. The number was {secretNumb}")
+            break;
+        elif guess > secretNumb:
+            print("Your guess was too high")
+        elif guess < secretNumb:
+            print("Your guess was too low")
+
+        if guessCount == guessLimit:
+            print(F"Too many tries. The number was {secretNumb}")
+
+    print("Game over")
+    i = input("Would you like to go again (y/n): ")
+    if i.upper() == "N":
+        goAgain = False
+    
     
