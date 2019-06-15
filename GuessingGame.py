e3ff1d78873e033375eac03acee4c367c314299a
Mike Skrypek <mikeskrypekjr@comcast.net>
2019-06-14 19:51:57 -0500

    Make a guessing game in python

---
+++ GuessingGame.py
@@ -0,0 +1,31 @@
+import random
+
+
+secretNumb = random.randint(1, 9)
+
+guessCount = 0
+guessLimit = 3
+goAgain = True
+
+while goAgain:
+    print("Guess a number between 1 and 9")
+    while guessCount < guessLimit:
+        guess = int(input("Enter your guess: "))
+        guessCount += 1
+        if guess == secretNumb:
+            print(F"You guess correct. The number was {secretNumb}")
+            break;
+        elif guess > secretNumb:
+            print("Your guess was too high")
+        elif guess < secretNumb:
+            print("Your guess was too low")
+
+        if guessCount == guessLimit:
+            print(F"Too many tries. The number was {secretNumb}")
+
+    print("Game over")
+    i = input("Would you like to go again (y/n): ")
+    if i.upper() == "N":
+        goAgain = False
+    
+    
