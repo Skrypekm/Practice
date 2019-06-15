02f044a358f6006a15187087d6f1082bfb57ea75
Mike Skrypek <mikeskrypekjr@comcast.net>
2019-06-15 13:26:53 -0500

    Practice with python functions

--- General.py
+++ General.py
@@ -1,30 +1,7 @@
-import random
+def greetings(Fname, Lname):
+    print(F"welcome {Lname}, {Fname}")
+    return
 
-secretNumb = random.randint(1, 9)
 
-guessCount = 0
-guessLimit = 3
-goAgain = True
-
-while goAgain:
-    print("Guess a number between 1 and 9")
-    while guessCount < guessLimit:
-        guess = int(input("Enter your guess: "))
-        guessCount += 1
-        if guess == secretNumb:
-            print(F"You guess correct. The number was {secretNumb}")
-            break;
-        elif guess > secretNumb:
-            print("Your guess was too high")
-        elif guess < secretNumb:
-            print("Your guess was too low")
-
-        if guessCount == guessLimit:
-            print(F"Too many tries. The number was {secretNumb}")
-
-    print("Game over")
-    i = input("Would you like to go again (y/n): ")
-    if i.upper() == "N":
-        goAgain = False
-
+greetings(Lname="Mike", Fname="Skrypek")
 exit(0)
