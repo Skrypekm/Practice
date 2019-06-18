455c7d04c1bcd0e9f60364d78717b965a9a76b20
Mike Skrypek <mikeskrypekjr@comcast.net>
2019-06-17 21:48:07 -0500

    finished python practice problem

--- General.py
+++ General.py
@@ -1,7 +1,32 @@
-def greetings(Fname, Lname):
-    print(F"welcome {Lname}, {Fname}")
-    return
+# Problem:
+# Read the amount of money you have and the prices of the items you intend to buy. Determine
+# whether you have enough money to buy everything you selected or whether you are short of
+# money. If you do not have enough money, indicate the amount of the shortfall. Be sure to
+# include 8% tax when figuring the amount you need.
 
 
-greetings(Lname="Mike", Fname="Skrypek")
+num = input("Enter array of numbers: ")
+numArr = num.split(' ')
+
+# determine the amount you have and the amount you owe
+first = True
+owed = 0
+
+for i in numArr:
+    if first:
+        owned = float(i)
+        first = False
+        continue
+    elif i == "-1":
+        break
+
+    owed += float(i)
+
+owed += owed * .08  # add tax
+
+if owned > owed:
+    print("ENOUGH MONEY")
+else:
+    print("$" + str(float(owed) - float(owned)) + " SHORT")
+
 exit(0)
