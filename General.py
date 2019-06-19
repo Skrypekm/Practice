f585d6687ad0420d01770f6050211054b877198a
Mike Skrypek <mikeskrypekjr@comcast.net>
2019-06-18 20:52:35 -0500

    Practice with creating and importing packages

--- General.py
+++ General.py
@@ -1,7 +1,10 @@
 import Converters
-from Converters import kg_to_lbs
+from Converters import kg_to_lbs as yes
+import ecommerce.shipping as shipping
 
-kg_to_lbs(100)
+shipping.calc_shipping()
+
+print(yes(100))
 
 print(Converters.kg_to_lbs(70))
 exit(0)
