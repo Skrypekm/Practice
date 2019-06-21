13be946272c1d3f107491f46795ee662f226370b
Mike Skrypek <mikeskrypekjr@comcast.net>
2019-06-21 16:22:14 -0500

    more testing with django

--- PyShop/products/views.py
+++ PyShop/products/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('This page is for new products')
+
